415ac3bac01f29027610482ff7ba7daf0e0005de
Jusong Yu <jusong.yeu@gmail.com>
2022-11-30 14:58:44 +0100

    mock_db fixture

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,18 +4,23 @@ import pytest
 
 from hpc_gateway.model import database
 
+@pytest.fixture(scope="function")
 @mongomock.patch(servers=(('server.example.com', 27017),))
-def test_create_user(monkeypatch):
+def mock_db():
     client = pymongo.MongoClient('server.example.com')
-    fake_db = client._database
+    db = client._database
     
-    monkeypatch.setattr('hpc_gateway.model.database.db', fake_db)
+    return db
+
+@mongomock.patch(servers=(('server.example.com', 27017),))
+def test_create_user(monkeypatch, mock_db):
+    monkeypatch.setattr('hpc_gateway.model.database.db', mock_db)
     
     name = "test"
     email = "test@test.com"
     user = database.create_user(email, name)
     
-    find_user = fake_db.users.find_one({"email": email})
+    find_user = mock_db.users.find_one({"email": email})
     
     assert user.inserted_id == find_user['_id']
     assert find_user['name'] == name
